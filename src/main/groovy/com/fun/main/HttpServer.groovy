b79d60008d3c5a881cd3133db08eadc12b4f6a69
fankaiqiang <fankaiqiang@okay.cn>
2019-05-31 10:59:20 +0800

    moco使用案例

---
+++ src/main/groovy/com/fun/main/HttpServer.groovy
@@ -0,0 +1,18 @@
+package com.fun.main
+
+import com.fun.base.bean.Result
+import com.fun.moco.MocoResponse
+import com.fun.moco.MocoServer
+
+class HttpServer extends MocoServer{
+    public static void main(String[] args) {
+        def server = getServer(getLogMonitor("1.log"))
+        server.get(urlOnly("/test")).response(obResponse(Result.success(getJson("324324=32432"))))
+        server.response(MocoResponse.contentResponse("hello word"))
+        def run = run(server)
+
+
+        waitForKey("fan")
+        run.stop()
+    }
+}
