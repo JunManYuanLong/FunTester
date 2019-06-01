cc073a3322c88b45ebee9773e74a7941eafa94df
fankaiqiang <fankaiqiang@okay.cn>
2019-06-01 08:48:53 +0800

    增加使用案例

--- src/main/groovy/com/fun/main/HttpServer.groovy
+++ src/main/groovy/com/fun/main/HttpServer.groovy
@@ -4,10 +4,13 @@ import com.fun.base.bean.Result
 import com.fun.moco.MocoResponse
 import com.fun.moco.MocoServer
 
-class HttpServer extends MocoServer{
+@SuppressWarnings("all")
+class HttpServer extends MocoServer {
     public static void main(String[] args) {
         def server = getServer(getLogMonitor("1.log"))
         server.get(urlOnly("/test")).response(obResponse(Result.success(getJson("324324=32432"))))
+        server.get(urlOnly("/ss")).response(success(getJson("324324=32432")))
+
         server.response(MocoResponse.contentResponse("hello word"))
         def run = run(server)
 
