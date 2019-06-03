5eefc4058f0cb4b89418e5ce811af0aa8bd6f297
fankaiqiang <fankaiqiang@okay.cn>
2019-06-03 10:00:32 +0800

    增加limit response的功能演示

--- src/main/groovy/com/fun/main/HttpServer.groovy
+++ src/main/groovy/com/fun/main/HttpServer.groovy
@@ -4,12 +4,15 @@ import com.fun.base.bean.Result
 import com.fun.moco.MocoResponse
 import com.fun.moco.MocoServer
 
+/**
+ * moco API的应用
+ */
 @SuppressWarnings("all")
 class HttpServer extends MocoServer {
     public static void main(String[] args) {
         def server = getServer(getLogMonitor("1.log"))
         server.get(urlOnly("/test")).response(obResponse(Result.success(getJson("324324=32432"))))
-        server.get(urlOnly("/ss")).response(success(getJson("324324=32432")))
+        server.get(urlOnly("/ss")).response(limit(contentResponse("dsfsd"),contentResponse("432432"),1000))
 
         server.response(MocoResponse.contentResponse("hello word"))
         def run = run(server)
