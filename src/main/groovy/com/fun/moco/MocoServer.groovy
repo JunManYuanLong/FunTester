9d740ff9cd518014570b4d61336dc22aace39fa8
fankaiqiang <fankaiqiang@okay.cn>
2019-06-05 18:24:27 +0800

    增加了注释

--- src/main/groovy/com/fun/moco/MocoServer.groovy
+++ src/main/groovy/com/fun/moco/MocoServer.groovy
@@ -8,6 +8,7 @@ import com.github.dreamhead.moco.Runner
 
 import static com.github.dreamhead.moco.Moco.httpServer
 import static com.github.dreamhead.moco.Moco.log
+
 /**
  * 获取server的工具类，提供了计数监视器和日志监视器
  */
@@ -16,32 +17,49 @@ class MocoServer extends MocoResponse {
     def array = new ArrayList()
 
 /**
- * 默认方法，端口号12345
+ * 获取httpserver对象，端口号12345
  * @return
  */
     static HttpServer getServer() {
         httpServer 12345, getLogMonitor()
     }
 
+/**
+ * 获取httpserver对象
+ * @param port
+ * @return
+ */
     static HttpServer getServer(int port) {
         httpServer port, getLogMonitor()
     }
 
+/**
+ * 获取httpserver对象
+ * @param mocoMonitors
+ * @return
+ */
     static HttpServer getServer(MocoMonitor mocoMonitors) {
         httpServer 12345, mocoMonitors
     }
 
-    static HttpServer getServer(final int port, String name, MocoMonitor configs) {
-        httpServer port, getLogMonitor(name), configs
+/**
+ * 获取httpserver对象
+ * @param port 端口
+ * @param logName 日志文件名
+ * @param configs 配置
+ * @return
+ */
+    static HttpServer getServer(final int port, String logName, MocoMonitor configs) {
+        httpServer port, getLogMonitor(logName), configs
     }
 
 /**
- * 获取日志监视器
- * @param name
+ * 获取日志监视器，在log_path下面
+ * @param logName
  * @return
  */
-    static def getLogMonitor(String name) {
-        log LOG_Path + name, DEFAULT_CHARSET
+    static def getLogMonitor(String logName) {
+        log LOG_Path + logName, DEFAULT_CHARSET
     }
 
 /**
@@ -53,7 +71,7 @@ class MocoServer extends MocoResponse {
     }
 
 /**
- * 获取计数监视器
+ * 获取计数监视器，计数器在做测试的时候用到，确认服务启动且接口调用正常
  * @return
  */
     static def getHitMonitor() {
