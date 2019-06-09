c563566e6b9533abc0a2413b7b4a19534ce4fc9f
fankaiqiang <fankaiqiang@okay.cn>
2019-06-09 15:33:44 +0800

    增加json格式返回案例

--- src/main/groovy/com/fun/main/HttpServer.groovy
+++ src/main/groovy/com/fun/main/HttpServer.groovy
@@ -13,10 +13,12 @@ import static com.github.dreamhead.moco.Moco.from
 class HttpServer extends MocoServer {
     public static void main(String[] args) {
         def server = getServer(getLogMonitor("1.log"))
-        server.get(urlOnly("/test")).response(obResponse(Result.success(getJson("324324=32432"))))
-        server.get(urlOnly("/ss")).response(limit(contentResponse("dsfsd"),contentResponse("432432"),1000))
+        server.get(urlOnly("/test")).response(obRes(Result.success(getJson("324324=32432"))))
+        server.get(urlOnly("/ss")).response(limit(textRes("dsfsd"),textRes("432432"),1000))
 
-        server.response(MocoResponse.contentResponse("hello word"))
+        server.get(urlOnly("/find")).response(random("43242=342","e343=fdsf","3242"))
+        server.get(urlOnly("/qq")).response(cycle(getJson("3234=32423"),getJson("ew=32")))
+        server.response(MocoResponse.textRes("hello word"))
         def run = run(server)
 
 
