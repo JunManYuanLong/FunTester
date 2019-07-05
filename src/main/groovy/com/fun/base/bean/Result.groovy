95f831bbddbb07ae8a2749889fd1b1696659b82d
fankaiqiang <fankaiqiang@okay.cn>
2019-07-05 11:37:11 +0800

    增加文档

--- src/main/groovy/com/fun/base/bean/Result.groovy
+++ src/main/groovy/com/fun/base/bean/Result.groovy
@@ -1,35 +1,57 @@
 package com.fun.base.bean
 /**
  * 通用的返回体
- * @param <T>
+ * 配合moco框架使用
+ * @param < T >
  */
 class Result<T> extends AbstractBean {
-
+    /**
+     * code码
+     */
     int code
-
+    /**
+     * 返回信息
+     */
     T data
 
     Result(int code, T data) {
         this.code = code
         this.data = data
     }
-
+/**
+ * 返回简单的响应
+ * @param c
+ */
     Result(int c) {
         this.code = c
     }
 
 
+/**
+ * 返回成功响应内容
+ * @param data
+ * @return
+ */
     static <T> Result<T> success(T data) {
         new Result<>(0, data)
     }
 
-    Result() {}
+    def Result() {}
 
+/**
+ * 返回通用失败的响应内容
+ * @param data
+ * @return
+ */
     static <T> Result<T> fail(T data) {
         new Result<T>(TEST_ERROR_CODE, data)
     }
 
-    boolean success() {
+/**
+ * 是否成功响应
+ * @return
+ */
+    boolean isSuccess() {
         code == 0
     }
 }
