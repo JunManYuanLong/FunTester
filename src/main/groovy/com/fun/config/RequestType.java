d85e7fd5da0bc7496c381ae791f056d24f710456
fankaiqiang <fankaiqiang@okay.cn>
2019-08-09 15:05:58 +0800

    增加日志

--- src/main/groovy/com/fun/config/RequestType.java
+++ src/main/groovy/com/fun/config/RequestType.java
@@ -1,10 +1,16 @@
 package com.fun.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 请求枚举类，fun备用，在区分post请求参数类型中用到
+ * fun是为了区分post请求，json格式传参用
  */
 public enum RequestType {
     GET("get"), POST("post"), FUN("fun");
+
+    static Logger logger = LoggerFactory.getLogger(RequestType.class);
 
     String name;
 
@@ -13,6 +19,7 @@ public enum RequestType {
     }
 
     public static RequestType getRequestType(String name) {
+        logger.debug("验证请求方式：{}", name);
         for (RequestType requestType : RequestType.values()) {
             if (requestType.name.equalsIgnoreCase(name)) {
                 return requestType;
