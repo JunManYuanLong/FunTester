192539bc33c52e4456f37b0ac12636a9dfb547fe
fankaiqiang <fankaiqiang@okay.cn>
2019-08-13 17:19:57 +0800

    增加获取文件最后几行的方法

--- src/main/groovy/com/fun/utils/CMD.java
+++ src/main/groovy/com/fun/utils/CMD.java
@@ -1,13 +1,11 @@
 package com.fun.utils;
 
 import com.fun.frame.SourceCode;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.Charset;
 
 /**
@@ -32,7 +30,7 @@ public class CMD extends SourceCode {
      * @param cmd 需要执行的命令
      */
     public static int execCmd(String cmd, Charset charset) {
-        logger.info("执行命令：", cmd);
+        logger.info("执行命令：{}", cmd);
         Process p = null;// 通过runtime类执行cmd命令
         try {
             p = Runtime.getRuntime().exec(cmd);
@@ -40,7 +38,12 @@ public class CMD extends SourceCode {
             logger.error("cmd：{}命令错误", e);
             return 1;
         }
-        try (InputStream input = p.getInputStream(); BufferedReader reader = new BufferedReader(new InputStreamReader(input, charset)); InputStream errorInput = p.getErrorStream(); BufferedReader errorReader = new BufferedReader(new InputStreamReader(errorInput))) {
+        try (InputStream input = p.getInputStream();
+             InputStreamReader inputStreamReader = new InputStreamReader(input, charset);
+             BufferedReader reader = new BufferedReader(inputStreamReader);
+             InputStream errorInput = p.getErrorStream();
+             InputStreamReader streamReader = new InputStreamReader(errorInput);
+             BufferedReader errorReader = new BufferedReader(streamReader)) {
             String line = "";
             while ((line = reader.readLine()) != null) {// 循环读取
                 output(line);// 输出
@@ -53,6 +56,35 @@ public class CMD extends SourceCode {
         } catch (IOException e) {
             logger.warn("执行" + cmd + "失败！", e);
             return 1;
+        }
+    }
+
+    /**
+     * 获取文本信息的最后几行，用户查看日志
+     *
+     * @param path
+     * @param num
+     * @return
+     */
+    public static String catFile(String path, int num) {
+        logger.info("查询的文件：{}", path);
+        if (StringUtils.isEmpty(path)) return EMPTY;
+        File file = new File(path);
+        if (!file.exists() || file.isDirectory()) return EMPTY;
+        StringBuffer stringBuffer = new StringBuffer();
+        String command = "tail -n " + num + SPACE_1 + path;
+        logger.info("执行命令：{}", command);
+        try (InputStream input = Runtime.getRuntime().exec(command).getInputStream();
+             InputStreamReader inputStreamReader = new InputStreamReader(input, DEFAULT_CHARSET);
+             BufferedReader reader = new BufferedReader(inputStreamReader);) {
+            String line = EMPTY;
+            while ((line = reader.readLine()) != null) {// 循环读取
+                stringBuffer.append(line + LINE);
+            }
+        } catch (IOException e) {
+            logger.error("获取：{}文件信息失败！", path, e);
+        } finally {
+            return stringBuffer.toString();
         }
     }
 }
