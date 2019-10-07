95f3eae447bed97ae9e36124061409f632262779
fankaiqiang <fankaiqiang@okay.cn>
2019-10-07 20:28:38 +0800

    修复小写汉字数字的BUG

--- src/main/groovy/com/fun/utils/RString.groovy
+++ src/main/groovy/com/fun/utils/RString.groovy
@@ -7,7 +7,7 @@ class RString extends SourceCode {
 
     static char[] chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', (char) 65, (char) 66, (char) 67, (char) 68, (char) 69, (char) 70, (char) 71, (char) 72, (char) 73, (char) 74, (char) 75, (char) 76, (char) 77, (char) 78, (char) 79, (char) 80, (char) 81, (char) 82, (char) 83, (char) 84, (char) 85, (char) 86, (char) 87, (char) 88, (char) 89, (char) 90, (char) 97, (char) 98, (char) 99, (char) 100, (char) 101, (char) 102, (char) 103, (char) 104, (char) 105, (char) 106, (char) 107, (char) 108, (char) 109, (char) 110, (char) 111, (char) 112, (char) 113, (char) 114, (char) 115, (char) 116, (char) 117, (char) 118, (char) 119, (char) 120, (char) 121, (char) 122]
 
-    static String[] chineses = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"]
+    static String[] chineses = ["〇", "一", "二", "三", "四", "五", "六", "七", "八", "九"]
 
     static String[] capeChineses = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"]
 
