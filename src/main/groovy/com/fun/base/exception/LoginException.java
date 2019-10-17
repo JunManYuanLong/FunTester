dab4b5ec000ae98753c46dfffa8c678e12160d63
fankaiqiang <fankaiqiang@okay.cn>
2019-10-17 11:16:45 +0800

    修改文件,增加无参方法

--- src/main/groovy/com/fun/base/exception/LoginException.java
+++ src/main/groovy/com/fun/base/exception/LoginException.java
@@ -2,10 +2,15 @@ package com.fun.base.exception;
 
 public class LoginException extends FailException {
 
-    private static final long serialVersionUID = 8674617502387938483L;
+	private static final long serialVersionUID = 8674617502387938483L;
 
-    public LoginException(String name) {
-        super(String.format("账号{%s}登录失败", name));
-    }
+	public LoginException() {
+		super("登录异常!");
+	}
+
+	public LoginException(String name) {
+		super(String.format("账号{%s}登录失败!", name));
+	}
+
 
 }
