9862d396db1436625ed10de4b14627748a7db241
fankaiqiang <fankaiqiang@okay.cn>
2019-10-22 11:07:56 +0800

    函数模拟demo

--- src/test/groovy/com/FunTester/mockito/practise/DemoJ.java
+++ src/test/groovy/com/FunTester/mockito/practise/DemoJ.java
@@ -1,5 +1,7 @@
 package com.FunTester.mockito.practise;
 
+import com.fun.frame.Output;
+import com.fun.frame.SourceCode;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -12,10 +14,11 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.AdditionalAnswers.returnsSecondArg;
+import static org.mockito.Mockito.*;
 
-public class DemoJ {
+public class DemoJ extends SourceCode {
+
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
 
@@ -32,4 +35,40 @@ public class DemoJ {
         final List<String> capturedArgument = captor.getValue();
         assertThat(capturedArgument, hasItem("someElement"));
     }
+
+    @Test
+    public final void answerTest() {
+        DemoJ mock = mock(DemoJ.class);
+        // with doAnswer():
+        doAnswer(returnsSecondArg()).when(mock).ds(anyInt(), anyInt());
+        // with thenAnswer():
+//        when(list.add(anyString())).thenAnswer(returnsFirstArg());
+        // with then() alias:
+//        when(list.add(anyString())).then(returnsFirstArg());
+        int ds = mock.ds(3, 2);
+        Output.output(ds);
+    }
+
+    @Test
+    public final void answerTest2() {
+        DemoJ mock = mock(DemoJ.class);
+        // with doAnswer():
+        when(mock.ds(anyInt(), anyInt())).thenAnswer(i -> {
+            Object argument = i.getArguments();
+            if (Integer.valueOf(argument.toString()) > 3) return 5;
+            return 2;
+        });
+        int ds = mock.ds(53, 2);
+        Output.output(ds);
+    }
+
+    public int ds(int i, int n) {
+        return i + n;
+    }
+
+
+    public int sss() {
+        return SourceCode.getMark();
+    }
+
 }
