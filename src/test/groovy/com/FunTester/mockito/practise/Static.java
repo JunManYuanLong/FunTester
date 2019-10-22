c0308b4b4870ed4a21d09faf43dfe4dd7922e892
fankaiqiang <fankaiqiang@okay.cn>
2019-10-22 15:44:48 +0800

    增加静态类的测试demo

--- src/test/groovy/com/FunTester/mockito/practise/Static.java
+++ src/test/groovy/com/FunTester/mockito/practise/Static.java
@@ -4,9 +4,15 @@ import com.fun.frame.SourceCode;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
+import org.mockito.AdditionalAnswers;
+import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(SourceCode.class)
@@ -15,10 +21,28 @@ public class Static {
     @Test
     public void testDD() {
         DemoJ demoJ = new DemoJ();
-        PowerMockito.mockStatic(SourceCode.class);
-        PowerMockito.when(SourceCode.getMark()).thenReturn(32334);
+        mockStatic(SourceCode.class);
+        when(SourceCode.getMark()).thenReturn(32334);
         int mark = demoJ.sss();
         Assert.assertTrue(mark == 32334);
+    }
+
+    @Test
+    public void testDS() {
+        mockStatic(SourceCode.class);
+        Mockito.when(SourceCode.changeStringToInt(anyString())).thenAnswer(i -> {
+            return i.getArgument(0).toString().length();
+        });
+        int i = SourceCode.changeStringToInt("3");
+        Assert.assertTrue(i == 1);
+    }
+
+    @Test
+    public void testPercent() {
+        mockStatic(SourceCode.class);
+        when(SourceCode.getRandomInt(anyInt())).then(AdditionalAnswers.returnsFirstArg());
+        int anInt = SourceCode.getRandomInt(20);
+        Assert.assertTrue(anInt == 20);
     }
 
 
