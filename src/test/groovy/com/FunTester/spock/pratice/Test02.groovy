7cc4400e63adcbfa3f7a41ed50037178605a09c6
fankaiqiang <fankaiqiang@okay.cn>
2019-11-11 18:07:18 +0800

    增加Demo

--- src/test/groovy/com/FunTester/spock/pratice/Test02.groovy
+++ src/test/groovy/com/FunTester/spock/pratice/Test02.groovy
@@ -6,9 +6,22 @@ import spock.lang.Shared
 import spock.lang.Specification
 
 import static com.fun.config.Constant.getLongFile
-import static com.fun.frame.SourceCode.getLogger
+import static com.fun.frame.Output.output
+import static com.fun.frame.SourceCode.*
 
 class Test02 extends Specification {
+
+    @Shared
+    def properties = PropertyUtils.getLocalProperties(getLongFile("1"))
+
+    @Shared
+    def cc = Arrays.asList(properties.getArrays("c")).stream().map {x -> Integer.valueOf(x)}.collect() as List
+
+    @Shared
+    def bb = Arrays.asList(properties.getArrays("b")).stream().map {x -> Integer.valueOf(x)}.collect() as List
+
+    @Shared
+    def aa = Arrays.asList(properties.getArrays("a")).stream().map {x -> Integer.valueOf(x)}.collect() as List
 
     @Shared
     Logger logger = getLogger(Test02.class.getName())
@@ -45,14 +58,81 @@ class Test02 extends Specification {
     def "测试数据读写完成数据驱动"() {
         given:
         def a = 0
-        def properties = PropertyUtils.getLocalProperties(getLongFile("1"))
         def arrays = properties.getArrays("id")
         def s1 = properties.getPropertyInt("size1")
         def s2 = properties.getPropertyInt("size2")
-        def list = Arrays.asList(arrays).stream().filter{x -> x.length() > 1}.collect() as List
+        def list = Arrays.asList(arrays).stream().filter {x -> x.length() > 1}.collect() as List
+
         expect:
         s1 == arrays.size()
         s2 == list.size()
+    }
+
+    def "测试自定义对象操作"() {
+        given: "给一个自定义对象"
+        def per = new Per()
+        per.age = 23
+        per.name = "FunTester"
+        def a = per
+
+        expect:
+        a.name == "FunTester"
+
+    }
+
+    def "线程安全测试"() {
+        given: "多线程支持测试,此处线程数改成很大之后效果比较明显"
+        range(2).forEach {new Per().start()}
+        sleep(1000)
+        output(Per.i)
+        expect:
+        4 == Per.i
+    }
+
+    def "测试集合验证使用数据驱动"() {
+        given: "此处写的无法被where使用"
+
+        expect:
+        c * c == a * a + b * b
+
+        where:
+        c << cc
+        b << bb
+        a << aa
+    }
+
+
+/**
+ * 测试类
+ */
+    class Per extends Thread {
+
+        static int i
+
+        @Override
+        public void run() {
+            i++
+            sleep(100)
+            i++
+        }
+
+        Per() {
+        }
+
+        Per(String name, int age) {
+            this()
+            this.name = name
+            this.age = age
+        }
+
+        String name
+
+        int age
+
+        static def getAll() {
+            i
+        }
+
     }
 
 }
