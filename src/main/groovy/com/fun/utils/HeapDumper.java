55c68aa6aebf3490aa961f582be66fe39cfcc658
fankaiqiang <fankaiqiang@okay.cn>
2019-12-07 15:42:36 +0800

    增加程序获取JVM转储文件的工具类

---
+++ src/main/groovy/com/fun/utils/HeapDumper.java
@@ -0,0 +1,58 @@
+package com.fun.utils;
+
+import com.fun.frame.SourceCode;
+import com.sun.management.HotSpotDiagnosticMXBean;
+import org.slf4j.Logger;
+
+import javax.management.MBeanServer;
+import java.lang.management.ManagementFactory;
+
+public class HeapDumper extends SourceCode {
+
+    private static Logger logger = getLogger();
+
+    /**
+     * 这是HotSpot Diagnostic MBean的名称
+     */
+    private static final String HOTSPOT_BEAN_NAME = "com.sun.management:type=HotSpotDiagnostic";
+
+    /**
+     * 用于存储热点诊断MBean的字段
+     */
+    private static volatile HotSpotDiagnosticMXBean hotspotMBean;
+
+    /**
+     * 下载内存转储文件
+     *
+     * @param fileName 文件名,例如:heap.bin,不兼容路径,会在当前目录下生成
+     * @param live
+     */
+    public static void dumpHeap(String fileName, boolean live) {
+        initHotspotMBean();
+        try {
+            hotspotMBean.dumpHeap(fileName, live);
+        } catch (Exception e) {
+            logger.error("生成内存转储文件失败!", e);
+        }
+    }
+
+    /**
+     * 初始化热点诊断MBean
+     */
+    private static void initHotspotMBean() {
+        if (hotspotMBean == null) {
+            synchronized (HeapDumper.class) {
+                if (hotspotMBean == null) {
+                    try {
+                        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+                        hotspotMBean = ManagementFactory.newPlatformMXBeanProxy(server, HOTSPOT_BEAN_NAME, HotSpotDiagnosticMXBean.class);
+                    } catch (Exception e) {
+                        logger.error("初始化mbean失败!", e);
+                    }
+                }
+            }
+        }
+    }
+
+
+}
