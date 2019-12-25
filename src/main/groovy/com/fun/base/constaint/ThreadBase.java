212a1958e0a614e07301664ce45f5c6ded88c0b4
fankaiqiang <fankaiqiang@okay.cn>
2019-12-25 10:10:55 +0800

    增加了status默认返回值,防止BUG出现

--- src/main/groovy/com/fun/base/constaint/ThreadBase.java
+++ src/main/groovy/com/fun/base/constaint/ThreadBase.java
@@ -68,7 +68,7 @@ public abstract class ThreadBase<T> extends SourceCode implements Runnable, Seri
      * 拷贝对象方法,用于统计单一对象多线程调用时候的请求数和成功数,对于<T>的复杂情况,需要将T类型也重写clone方法
      *
      * <p>
-     *     此处若具体实现类而非虚拟类建议自己写clone方法
+     * 此处若具体实现类而非虚拟类建议自己写clone方法
      * </p>
      *
      * @return
@@ -79,14 +79,16 @@ public abstract class ThreadBase<T> extends SourceCode implements Runnable, Seri
     }
 
     /**
-     * 线程任务是否需要提前关闭
+     * 线程任务是否需要提前关闭,默认返回false
      * <p>
      * 一般用于单线程错误率过高的情况
      * </p>
      *
      * @return
      */
-    public abstract boolean status();
+    public boolean status() {
+        return false;
+    }
 
 
 }
