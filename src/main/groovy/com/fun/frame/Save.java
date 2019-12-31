69402fc12e224c1a96c822c9bfcdeb8db4f6afad
fankaiqiang <fankaiqiang@okay.cn>
2019-12-31 10:32:19 +0800

    修改参数,增加适用性

--- src/main/groovy/com/fun/frame/Save.java
+++ src/main/groovy/com/fun/frame/Save.java
@@ -8,8 +8,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 /**
  * 用来保存数据的类，如果文件已经存在会删除原来的文件
@@ -24,7 +24,7 @@ public class Save extends SourceCode {
      * @param content 内容
      */
     public static void info(String content) {
-        info("long.log", content);
+        info("long", content);
     }
 
     public static void info(String name, String content) {
@@ -35,27 +35,27 @@ public class Save extends SourceCode {
     }
 
     /**
-     * 保存list数据到本地文件，name不需要后缀
+     * 保存list数据到本地文件
      */
-    public static void saveLongList(List<Long> data, Object name) {
+    public static void saveLongList(Collection<Long> data, Object name) {
         List<String> list = new ArrayList<>();
         data.forEach(num -> list.add(num.toString()));
         saveStringList(list, name.toString());
     }
 
     /**
-     * 保存list数据到本地文件，name不需要后缀
+     * 保存list数据到本地文件
      */
-    public static void saveIntegerList(List<Integer> data, String name) {
+    public static void saveIntegerList(Collection<Integer> data, String name) {
         List<String> list = new ArrayList<>();
         data.forEach(num -> list.add(num.toString()));
         saveStringList(list, name);
     }
 
     /**
-     * 保存list数据到本地文件，name不需要后缀
+     * 保存list数据到本地文件
      */
-    public static void saveDoubleList(List<Double> data, String name) {
+    public static void saveDoubleList(Collection<Double> data, String name) {
         List<String> list = new ArrayList<>();
         data.forEach(num -> list.add(num.toString()));
         saveStringList(list, name);
@@ -67,30 +67,22 @@ public class Save extends SourceCode {
      * @param data
      * @param name
      */
-    public static void saveList(List<Object> data, String name) {
+    public static void saveList(Collection<Object> data, String name) {
         List<String> list = new ArrayList<>();
         data.forEach(num -> list.add(num.toString()));
         saveStringList(list, name);
     }
 
     /**
-     * 保存list数据到本地文件,name不需要后缀
+     * 保存list数据到本地文件
      */
-    public static void saveStringList(List<String> data, String name) {
+    public static void saveStringList(Collection<String> data, String name) {
         String join = StringUtils.join(data, LINE);
         info(name, join);
     }
 
     /**
-     * 保存set数据到本地文件,name不需要后缀
-     */
-    public static void saveStringList(Set<String> data, String name) {
-        List<String> objects = new ArrayList<>(data);
-        saveStringList(objects, name);
-    }
-
-    /**
-     * 保存json数据到本地文件，name不需要后缀
+     * 保存json数据到本地文件
      */
     public static void saveJson(JSONObject data, String name) {
         StringBuffer buffer = new StringBuffer();
