39d0273e288c868cfe43a8f09bc610d17ae254bb
fankaiqiang <fankaiqiang@okay.cn>
2020-01-02 18:16:06 +0800

    修正方法

--- src/main/groovy/com/fun/main/Blog.groovy
+++ src/main/groovy/com/fun/main/Blog.groovy
@@ -1,11 +1,13 @@
 package com.fun.main
 
 import com.fun.moco.MocoServer
+import com.fun.utils.ArgsUtil
 import com.github.dreamhead.moco.MocoMount
 
 class Blog extends MocoServer {
     public static void main(String[] args) {
-        def server = getServer(8080, "blog", getHitMonitor())
+        def util = new ArgsUtil(args)
+        def server = getServer(util.getIntOrdefault(0,8080), "blog", getHitMonitor())
         server.get(urlOnly("/")).redirectTo("/blog/home.html")
         server.mount("/blog/", MocoMount.to("/blog"))
         def server1 = run(server)
