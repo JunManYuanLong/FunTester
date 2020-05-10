2e7a5c8176d082f8aa1c5f720370fc48ba6f5125
fankaiqiang <fankaiqiang@okay.cn>
2020-05-10 14:48:34 +0800

    增加异步请求的方法封装

--- src/main/groovy/com/fun/frame/httpclient/FanLibrary.java
+++ src/main/groovy/com/fun/frame/httpclient/FanLibrary.java
@@ -35,6 +35,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 /**
@@ -269,7 +271,7 @@ public class FanLibrary extends SourceCode {
             jsonObject = getJson("content=" + content, "code=" + TEST_ERROR_CODE);
             logger.warn("响应体非json格式，已经自动转换成json格式！");
         } finally {
-            if (!cookies.isEmpty()) jsonObject.put(HttpClientConstant.COOKIE, cookies);
+            if (cookies != null && !cookies.isEmpty()) jsonObject.put(HttpClientConstant.COOKIE, cookies);
             return jsonObject;
         }
     }
@@ -281,7 +283,7 @@ public class FanLibrary extends SourceCode {
      * @param response
      * @return
      */
-    public static String getContent(CloseableHttpResponse response) {
+    public static String getContent(HttpResponse response) {
         HttpEntity entity = response.getEntity();// 获取响应实体
         String content = EMPTY;
         try {
@@ -490,6 +492,27 @@ public class FanLibrary extends SourceCode {
     public static void excuteSync(HttpRequestBase request) {
         if (!ClientManage.httpAsyncClient.isRunning()) ClientManage.httpAsyncClient.start();
         ClientManage.httpAsyncClient.execute(request, null);
+    }
+
+    /**
+     * 异步发送请求获取影响Demo
+     * <p>经过测试没卵用</p>
+     *
+     * @param request
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    public static JSONObject excuteSyncWithResponse(HttpRequestBase request) {
+        if (!ClientManage.httpAsyncClient.isRunning()) ClientManage.httpAsyncClient.start();
+        Future<HttpResponse> execute = ClientManage.httpAsyncClient.execute(request, null);
+        try {
+            HttpResponse httpResponse = execute.get();
+            String content = getContent(httpResponse);
+            return getJsonResponse(content, null);
+        } catch (Exception e) {
+            logger.error("异步请求获取响应失败!", e);
+        }
+        return new JSONObject();
     }
 
     /**
