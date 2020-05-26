2742f2912b191c709470acad2a3e8d38456db967
fankaiqiang <fankaiqiang@okay.cn>
2020-05-26 17:56:11 +0800

    增加文档

--- src/main/groovy/com/fun/moco/support/DelayHandler.groovy
+++ src/main/groovy/com/fun/moco/support/DelayHandler.groovy
@@ -5,10 +5,18 @@ import com.github.dreamhead.moco.ResponseHandler
 import com.github.dreamhead.moco.handler.AbstractResponseHandler
 import com.github.dreamhead.moco.internal.SessionContext
 
+import java.util.concurrent.TimeUnit
+
 import static com.google.common.base.Preconditions.checkArgument
 
+/**
+ * 延迟响应API拓展ResponseHandler
+ */
 class DelayHandler extends AbstractResponseHandler {
 
+    /**
+     * 延迟时间
+     */
     private final int time
 
     private final ResponseHandler handler
@@ -23,9 +31,13 @@ class DelayHandler extends AbstractResponseHandler {
         return new DelayHandler(handler, time);
     }
 
+/**
+ *
+ * @param context
+ */
     @Override
     void writeToResponse(SessionContext context) {
-        sleep(time)
+        com.github.dreamhead.moco.util.Idles.idle(time, TimeUnit.MILLISECONDS)
         handler.writeToResponse(context)
     }
 
