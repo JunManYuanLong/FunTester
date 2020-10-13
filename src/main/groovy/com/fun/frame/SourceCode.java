d3fe71b9bfaa5275305ed6105a29413faf097c26
fankaiqiang <fankaiqiang@okay.cn>
2020-10-13 16:53:51 +0800

    增加方法的适应性

--- src/main/groovy/com/fun/frame/SourceCode.java
+++ src/main/groovy/com/fun/frame/SourceCode.java
@@ -289,7 +289,7 @@ public class SourceCode extends Output implements Cloneable {
      * @param nanosec
      */
     public static void sleep(long nanosec) {
-        if (nanosec < 1000) return;
+        if (nanosec < 1_000_000) return;
         try {
             TimeUnit.NANOSECONDS.sleep(nanosec);
         } catch (InterruptedException e) {
