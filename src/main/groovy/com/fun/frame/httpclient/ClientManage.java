a71be4674cc54ec4910635d02215ff6cfc559c20
fankaiqiang <fankaiqiang@okay.cn>
2020-11-02 18:53:12 +0800

    修复可能进行的多余判断的问题.

--- src/main/groovy/com/fun/frame/httpclient/ClientManage.java
+++ src/main/groovy/com/fun/frame/httpclient/ClientManage.java
@@ -279,7 +279,7 @@ public class ClientManage extends SourceCode {
         HttpClientConstant.SOCKET_TIMEOUT = timeout;
         HttpClientConstant.MAX_ACCEPT_TIME = accepttime;
         HttpClientConstant.TRY_TIMES = retrytime;
-        requestConfig = StringUtils.isBlank(ip + ":" + port) || !Regex.isMatch(ip + ":" + port, "((25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d))):([0-9]|[1-9]\\d{1,3}|[1-5]\\d{4}|6[0-4]\\d{4}|65[0-4]\\d{2}|655[0-2]\\d|6553[0-5])") ? getProxyRequestConfig(ip, port) : getRequestConfig();
+        requestConfig = StringUtils.isBlank(ip) || !Regex.isMatch(ip + ":" + port, "((25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d))):([0-9]|[1-9]\\d{1,3}|[1-5]\\d{4}|6[0-4]\\d{4}|65[0-4]\\d{2}|655[0-2]\\d|6553[0-5])") ? getProxyRequestConfig(ip, port) : getRequestConfig();
         httpsClient = getCloseableHttpsClients();
         httpRequestRetryHandler = getHttpRequestRetryHandler();
     }
