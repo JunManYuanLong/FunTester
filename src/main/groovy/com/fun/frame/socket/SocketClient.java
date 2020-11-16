15654c0b05c903e76dfae347d1f536d48a055a0d
fankaiqiang <fankaiqiang@okay.cn>
2020-11-16 16:25:46 +0800

    增加socketclient代码

---
+++ src/main/groovy/com/fun/frame/socket/SocketClient.java
@@ -0,0 +1,111 @@
+package com.fun.frame.socket;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fun.base.bean.AbstractBean;
+import com.fun.base.exception.ParamException;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+
+/**
+ * socket客户端代码
+ */
+public class SocketClient extends WebSocketClient implements Serializable {
+
+    private static final long serialVersionUID = 1306796619468953402L;
+
+    private static Logger logger = LoggerFactory.getLogger(SocketClient.class);
+
+    private SocketClient(URI uri) {
+        super(uri);
+    }
+
+    public static SocketClient getInstance(String url) {
+        URI uri = null;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            ParamException.fail("创建socket client 失败! 原因:" + e.getMessage());
+        }
+        return new SocketClient(uri);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        logger.info("开始建立socket连接...");
+        handshakedata.iterateHttpFields().forEachRemaining(x -> logger.info("握手信息key: {} ,value: {}", x, handshakedata.getFieldValue(x)));
+    }
+
+    /**
+     * 收到消息时候调用的方法
+     *
+     * @param message
+     */
+    @Override
+    public void onMessage(String message) {
+        logger.info("收到: {}", message);
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        logger.info("socket 连接关闭...");
+        try {
+             this.getSocket().close();
+        } catch (IOException e) {
+            logger.error("socket连接关闭失败!", e);
+        }
+    }
+
+    @Override
+    public void onError(Exception e) {
+        logger.error("socket异常!", e);
+    }
+
+    @Override
+    public void send(String text) {
+        logger.debug("发送:{}", text);
+        super.send(text);
+    }
+
+    /**
+     * 发送非默认编码格式的文字
+     *
+     * @param text
+     * @param charset
+     */
+    public void send(String text, Charset charset) {
+        send(new String(text.getBytes(), charset));
+    }
+
+    /**
+     * 发送json信息
+     *
+     * @param json
+     */
+    public void send(JSONObject json) {
+        send(json.toJSONString());
+    }
+
+    /**
+     * 发送bean
+     *
+     * @param bean
+     */
+    public void send(AbstractBean bean) {
+        send(bean.toString());
+    }
+
+    @Override
+    public SocketClient clone() {
+        return new SocketClient(this.uri);
+    }
+
+
+}
