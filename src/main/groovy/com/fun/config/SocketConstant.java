cac9d82672563349725bd3afc097fd74c0701e6f
fankaiqiang <fankaiqiang@okay.cn>
2020-12-07 18:42:03 +0800

    增加注释

--- src/main/groovy/com/fun/config/SocketConstant.java
+++ src/main/groovy/com/fun/config/SocketConstant.java
@@ -5,12 +5,15 @@ package com.fun.config;
  */
 public class SocketConstant {
 
+    /* WebSocket独享配置     */
     public static int MAX_WATI_TIMES = 3;
 
+    /* 共享配置    */
     public static int WAIT_INTERVAL = 3;
 
     public static int MAX_MSG_SIZE = 20;
 
+    /*Socket.IO独享配置*/
     public static int MAX_RETRY = 3;
 
     public static int RETRY_DELAY = 1000;
@@ -18,5 +21,6 @@ public class SocketConstant {
     public static int TIMEOUT = 1000;
 
     public static String[] transports = {"websocket"};
+
 
 }
