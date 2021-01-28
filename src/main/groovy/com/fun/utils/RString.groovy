b18eb68d3cd05f0b1c07a1111ea385bddbca872c
fankaiqiang <fankaiqiang@okay.cn>
2021-01-28 10:49:21 +0800

    增加注释

--- src/main/groovy/com/fun/utils/RString.groovy
+++ src/main/groovy/com/fun/utils/RString.groovy
@@ -3,12 +3,21 @@ package com.fun.utils
 
 import com.fun.frame.SourceCode
 
+/**
+ * 处理各种随机字符串的工具类
+ */
 class RString extends SourceCode {
 
     private static final char[] chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', (char) 65, (char) 66, (char) 67, (char) 68, (char) 69, (char) 70, (char) 71, (char) 72, (char) 73, (char) 74, (char) 75, (char) 76, (char) 77, (char) 78, (char) 79, (char) 80, (char) 81, (char) 82, (char) 83, (char) 84, (char) 85, (char) 86, (char) 87, (char) 88, (char) 89, (char) 90, (char) 97, (char) 98, (char) 99, (char) 100, (char) 101, (char) 102, (char) 103, (char) 104, (char) 105, (char) 106, (char) 107, (char) 108, (char) 109, (char) 110, (char) 111, (char) 112, (char) 113, (char) 114, (char) 115, (char) 116, (char) 117, (char) 118, (char) 119, (char) 120, (char) 121, (char) 122]
 
+    /**
+     * 小写汉字数字
+     */
     private static final String[] chineses = ["〇", "一", "二", "三", "四", "五", "六", "七", "八", "九"]
 
+    /**
+     * 大写汉字数字
+     */
     private static final String[] capeChineses = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"]
 
     /**
@@ -60,7 +69,7 @@ class RString extends SourceCode {
      * 获取所有小写字母
      * @return
      */
-    static String getAllLowWord() {
+    static String getLowWords() {
         "abcdefghijklmnopqrstuvwxyz"
     }
 
@@ -68,7 +77,7 @@ class RString extends SourceCode {
      * 获取所有大写字母
      * @return
      */
-    static String getAllUpWord() {
+    static String getUpWords() {
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     }
 
@@ -76,7 +85,7 @@ class RString extends SourceCode {
      * 获取所有的数字
      * @return
      */
-    static String getAllNumber() {
+    static String getNumbers() {
         "0123456789"
     }
 
@@ -100,6 +109,5 @@ class RString extends SourceCode {
         if (i <= 0) return "零零零"
         def num = (i + EMPTY).collect {x -> capeChineses[changeStringToInt(x)]}.join()
         num.length() > 2 ? num : getManyString(capeChineses[0] + EMPTY, 3 - num.length()) + num
-
     }
 }
