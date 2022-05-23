93a710eb1390cbd4e9e5f85fc18d1f3b0b500acc
FunTester <FunTester@88.com>
2022-05-23 22:08:27 +0800

    增加对象池默认参数

---
+++ src/main/groovy/com/funtester/config/PoolConstant.groovy
@@ -0,0 +1,16 @@
+package com.funtester.config
+
+class PoolConstant {
+
+    public static int MAX = 100_000;
+
+    public static int MAX_IDLE = 88;
+
+    public static int MIN_IDLE = 8;
+
+    public static int MAX_WAIT_TIME = 1024;
+
+    public static int MAX_IDLE_TIME = 8 * 60 * 1000;
+
+
+}
