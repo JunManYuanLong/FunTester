80bf502295e427c87e7de54bd2030cb2763651cb
kaiqiang.fan <kaiqiang.fan@okg.com>
2022-12-16 19:15:16 +0800

    回复JsonPath功能

--- src/main/groovy/com/funtester/utils/JsonUtil.groovy
+++ src/main/groovy/com/funtester/utils/JsonUtil.groovy
@@ -1,4 +1,14 @@
 package com.funtester.utils
+
+import com.alibaba.fastjson.JSONObject
+import com.funtester.base.exception.ParamException
+import com.funtester.config.Constant
+import com.funtester.frame.JsonVerify
+import com.funtester.frame.SourceCode
+import com.jayway.jsonpath.JsonPath
+import com.jayway.jsonpath.JsonPathException
+import groovy.util.logging.Log4j2
+
 /**下面是例子,官方文档地址:https://github.com/json-path/JsonPath/blob/master/README.md
  * $.store.book[*].author	The authors of all books
  * $..author	All authors
@@ -49,112 +59,111 @@ package com.funtester.utils
  * groovy需要  \$  Java不需要直接  $
  *
  */
+@Log4j2
 class JsonUtil {
 
-//    private static Logger logger = LogManager.getLogger(JsonUtil.class)
-//
-//    /**
-//     * 用户构建对象,获取verify对象
-//     */
-//    private JSONObject json
-//
-//    private JsonUtil(JSONObject json) {
-//        this.json = json
-//    }
-//
-//    static JsonUtil getInstance(JSONObject json) {
-//        new JsonUtil(json)
-//    }
-//
-//    JsonVerify getVerify(String path) {
-//        JsonVerify.getInstance(this.json, path)
-//    }
-//
-//    /**
-//     * 获取string对象
-//     * @param path
-//     * @return
-//     */
-//    String getString(String path) {
-//        def object = get(path)
-//        object == null ? Constant.EMPTY : object.toString()
-//    }
-//
-//
-//    /**
-//     * 获取int类型
-//     * @param path
-//     * @return
-//     */
-//    int getInt(String path) {
-//        SourceCode.changeStringToInt(getString ( path))
-//    }
-//
-//    /**
-//     * 获取boolean类型
-//     * @param path
-//     * @return
-//     */
-//    int getBoolean(String path) {
-//        SourceCode.changeStringToBoolean(getString(path))
-//    }
-//
-//    /**
-//     * 获取long类型
-//     * @param path
-//     * @return
-//     */
-//    int getLong(String path) {
-//        SourceCode.changeStringToLong(getString(path))
-//    }
-//
-//    /**
-//     * 获取double类型
-//     * @param path
-//     * @return
-//     */
-//    double getDouble(String path) {
-//        SourceCode.changeStringToDouble(getString(path))
-//    }
-//
-//    /**
-//     * 获取list对象
-//     * @param path
-//     * @return
-//     */
-//    List getList(String path) {
-//        get(path) as List
-//    }
-//
-//    /**
-//     * 获取匹配对象,类型传参
-//     * 这里不加public  IDE会报错
-//     * @param path
-//     * @param tClass
-//     * @return
-//     */
-//    public <T> T getT(String path, Class<T> tClass) {
-//        try {
-//            get(path) as T
-//        } catch (ClassCastException e) {
-//            logger.warn("类型转换失败!", e)
-//        }
-//    }
-//
-//    /**
-//     * 获取匹配对象,这里如果使用模糊路径匹配失败,返回"[]",如果绝对路径报错
-//     * @param path
-//     * @return
-//     */
-//    def get(String path) {
-//        logger.debug("匹配对象:{},表达式:{}", json.toString(), path)
-//        if (json == null || json.isEmpty()) ParamException.fail("json为空或者null,参数错误!")
-//        try {
-//            JsonPath.read(this.json, path)
-//        } catch (JsonPathException e) {
-//            logger.warn("json: {} 解析失败,path: {}", json.toString(), path, e)
-//        }
-//    }
-//
+    /**
+     * 用户构建对象,获取verify对象
+     */
+    private JSONObject json
+
+    private JsonUtil(JSONObject json) {
+        this.json = json
+    }
+
+    static JsonUtil getInstance(JSONObject json) {
+        new JsonUtil(json)
+    }
+
+    JsonVerify getVerify(String path) {
+        JsonVerify.getInstance(this.json, path)
+    }
+
+    /**
+     * 获取string对象
+     * @param path
+     * @return
+     */
+    String getString(String path) {
+        def object = get(path)
+        object == null ? Constant.EMPTY : object.toString()
+    }
+
+
+    /**
+     * 获取int类型
+     * @param path
+     * @return
+     */
+    int getInt(String path) {
+        SourceCode.changeStringToInt(getString ( path))
+    }
+
+    /**
+     * 获取boolean类型
+     * @param path
+     * @return
+     */
+    int getBoolean(String path) {
+        SourceCode.changeStringToBoolean(getString(path))
+    }
+
+    /**
+     * 获取long类型
+     * @param path
+     * @return
+     */
+    int getLong(String path) {
+        SourceCode.changeStringToLong(getString(path))
+    }
+
+    /**
+     * 获取double类型
+     * @param path
+     * @return
+     */
+    double getDouble(String path) {
+        SourceCode.changeStringToDouble(getString(path))
+    }
+
+    /**
+     * 获取list对象
+     * @param path
+     * @return
+     */
+    List getList(String path) {
+        get(path) as List
+    }
+
+    /**
+     * 获取匹配对象,类型传参
+     * 这里不加public  IDE会报错
+     * @param path
+     * @param tClass
+     * @return
+     */
+    public <T> T getT(String path, Class<T> tClass) {
+        try {
+            get(path) as T
+        } catch (ClassCastException e) {
+            log.warn("类型转换失败!", e)
+        }
+    }
+
+    /**
+     * 获取匹配对象,这里如果使用模糊路径匹配失败,返回"[]",如果绝对路径报错
+     * @param path
+     * @return
+     */
+    def get(String path) {
+        log.debug("匹配对象:{},表达式:{}", json.toString(), path)
+        if (json == null || json.isEmpty()) ParamException.fail("json为空或者null,参数错误!")
+        try {
+            JsonPath.read(this.json, path)
+        } catch (JsonPathException e) {
+            log.warn("json: {} 解析失败,path: {}", json.toString(), path, e)
+        }
+    }
+
 
 }
