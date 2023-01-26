58a931ef06c3c9b68e1fc6553db2d53664b47dba
kaiqiang.fan <kaiqiang.fan@okg.com>
2023-01-26 20:33:24 +0800

    丰富系统功能

--- src/main/groovy/com/funtester/utils/OSUtil.groovy
+++ src/main/groovy/com/funtester/utils/OSUtil.groovy
@@ -1,20 +1,24 @@
 package com.funtester.utils
 
+import com.funtester.base.bean.AbstractBean
 import com.funtester.frame.SourceCode
 
-import java.lang.management.ManagementFactory
-import java.lang.management.OperatingSystemMXBean
-import java.lang.management.ThreadMXBean
+import java.lang.management.*
 
 class OSUtil extends SourceCode {
 
     static OperatingSystemMXBean osMxBean = ManagementFactory.getOperatingSystemMXBean()
+
+    static MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean()
 
     static ThreadMXBean threadBean = ManagementFactory.getThreadMXBean()
 
     static long lastSysTime = System.nanoTime()
 
     static long lastUserTime = 0
+
+    static List<GarbageCollectorMXBean> gcMxBeans = ManagementFactory.getGarbageCollectorMXBeans();
+
 
     /**
      * 获取最大进程数N,CPU使用率N*100%
@@ -39,8 +43,68 @@ class OSUtil extends SourceCode {
         long totalPassedTime = curtime - lastSysTime
         lastSysTime = curtime
         lastUserTime = totalTime
-        def d = (((double) usedTime) / totalPassedTime / osMxBean.getAvailableProcessors()) * 100
+        def d = (((double) usedTime) / totalPassedTime / getAvailableProcessors()) * 100
         return d > 100 ? 8.88 : d
     }
 
+    /**
+     * 获取堆内存信息
+     * @return
+     */
+    static def heapMemInfo() {
+        memoryMXBean.getHeapMemoryUsage()
+    }
+
+    /**
+     * 获取非堆内存信息
+     * @return
+     */
+    static def noHeapMemInfo() {
+        memoryMXBean.getNonHeapMemoryUsage()
+    }
+
+    /**
+     * 获取系统一分钟内的平均load
+     * @return
+     */
+    static def getLoad() {
+        osMxBean.getSystemLoadAverage() / getAvailableProcessors()
+    }
+
+    /**
+     * 获取GC信息{@link com.funtester.utils.OSUtil.GCInfo}
+     * @return
+     */
+    static def getGCinfo() {
+        def infos = []
+        for (GarbageCollectorMXBean gcMxBean : gcMxBeans) {
+            infos << new GCInfo(gcMxBean)
+        }
+        infos
+    }
+
+    /**
+     * GC信息类
+     */
+    static class GCInfo extends AbstractBean {
+
+        String name
+
+        int count
+
+        int time
+
+        GCInfo(String name, int count, int time) {
+            this.name = name
+            this.count = count
+            this.time = time
+        }
+
+        GCInfo(GarbageCollectorMXBean gcMxBean) {
+            this.name = gcMxBean.getName()
+            this.count = gcMxBean.getCollectionCount()
+            this.time = gcMxBean.getCollectionTime()
+        }
+
+    }
 }
