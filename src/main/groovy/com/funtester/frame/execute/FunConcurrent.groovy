54650c6549030ce48e20a0e29add73475c96f47a
kaiqiang.fan <kaiqiang.fan@okg.com>
2023-01-27 20:04:30 +0800

    丰富命令行的功能

--- src/main/groovy/com/funtester/frame/execute/FunConcurrent.groovy
+++ src/main/groovy/com/funtester/frame/execute/FunConcurrent.groovy
@@ -118,10 +118,59 @@ class FunConcurrent extends SourceCode {
                         over()
                         break
                     default:
-                        if (Regex.isMatch(input, "(F|f)\\d+")) THREAD_STEP = changeStringToInt(input.substring(1))
-                        break
+                        if (Regex.isMatch(input, "(F|f)\\d+")) QPS_STEP = changeStringToInt(input.substring(1));
+                        if (Regex.isMatch(input, "(T|t)\\d+(D|d)\\d+")) {
+                            def split = (input - "T" - "t").split(/(d|D)/)
+                            autoTarget(split[0] as int, split[1] as int)
+                        }
+                        if (Regex.isMatch(input, "(A|a)-{0,1}\\d+(D|d)\\d+")) {
+                            def split = (input - "A" - "a").split(/(d|D)/)
+                            autoAdd(split[0] as int, split[1] as int)
+                        }
+                        break;
                 }
                 FunThread.printInfo()
+            }
+        }
+
+        /**
+         * 自动控制递增功能,以目标值计算
+         * @param target 目标QPS
+         * @param duration 持续时间
+         * @return
+         */
+        def autoTarget(int target, duration) {
+            fun {
+                int q = qps
+                def interval = (target - qps) * 1.0 / duration
+                for (i in 0..<duration) {
+                    if (autoKey) break
+                    qps = (q + interval * (i + 1)) as int
+                    if (qps < 0) break
+                    sleep(1.0)
+                }
+                if (!autoKey) qps = target
+                autoKey = false
+            }
+        }
+
+        /**
+         * 自动控制递增功能,以增加值计算
+         * @param sum
+         * @param duration
+         * @return
+         */
+        def autoAdd(int sum, duration) {
+            fun {
+                int q = qps
+                def interval = sum * 1.0 / duration
+                for (i in 0..<duration) {
+                    if (autoKey) break
+                    qps = (q + interval * (i + 1)) as int
+                    sleep(1.0)
+                }
+                if (!autoKey) qps = q + sum
+                autoKey = false
             }
         }
 
