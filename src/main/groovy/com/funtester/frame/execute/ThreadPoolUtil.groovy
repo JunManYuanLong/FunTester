08a33d2435cdeb9f0a20bf4bbd9d97c451eb6b68
kaiqiang.fan <kaiqiang.fan@okg.com>
2023-03-03 16:23:36 +0800

    修复拼写错误

--- src/main/groovy/com/funtester/frame/execute/ThreadPoolUtil.groovy
+++ src/main/groovy/com/funtester/frame/execute/ThreadPoolUtil.groovy
@@ -286,7 +286,7 @@ class ThreadPoolUtil extends Constant {
             }
         })
         thread.setDaemon(true)
-        thread.setName("Deamon")
+        thread.setName("Daemon")
         thread.start()
     }
 
