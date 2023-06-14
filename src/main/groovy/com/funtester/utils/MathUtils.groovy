9413640dbc453dd7a1d4c2be112cc2ed4f2cbde2
kaiqiang.fan <kaiqiang.fan@okg.com>
2023-06-14 13:58:37 +0800

    handle exception

--- src/main/groovy/com/funtester/utils/MathUtils.groovy
+++ src/main/groovy/com/funtester/utils/MathUtils.groovy
@@ -1,7 +1,12 @@
 package com.funtester.utils
 
+import com.funtester.config.Constant
+import com.funtester.frame.SourceCode
+import groovy.util.logging.Log4j2
+
 import java.util.stream.Collectors
 
+@Log4j2
 class MathUtils {
 
     /**
@@ -28,9 +33,15 @@ class MathUtils {
      * @param numbers
      * @return
      */
-    static def coefficientOfVariation(List<? extends Number> numbers) {
-        double avg = numbers.average()
-        Math.sqrt(numbers.stream().map {Math.pow((it - avg), 2)}.collect(Collectors.toList()).average()) / avg
+    static double coefficientOfVariation(List<? extends Number> numbers) {
+        try {
+            double avg = numbers.average()
+            def variation = Math.sqrt(numbers.stream().map {Math.pow((it - avg), 2)}.collect(Collectors.toList()).average()) / avg
+            SourceCode.formatDouble(variation) as double
+        } catch (e) {
+            log.warn("count coefficientOfVariation fail", numbers)
+            Constant.TEST_ERROR_CODE
+        }
     }
 
 }
