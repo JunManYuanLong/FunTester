c0a061ae797c06b57fea3a36447049bbfbbc4508
kaiqiang.fan <kaiqiang.fan@okg.com>
2023-10-27 09:25:26 +0800

    enhance function

--- src/main/groovy/com/funtester/utils/Time.java
+++ src/main/groovy/com/funtester/utils/Time.java
@@ -272,7 +272,12 @@ public class Time {
      * @return
      */
     public static long getZeroTimestamp() {
-        long timeStamp = getTimeStamp();
-        return timeStamp - timeStamp % Constant.DAY - Calendar.getInstance().getTimeZone().getRawOffset();
+        Calendar cal = Time.calendarInit();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTimeInMillis();
     }
+
 }
