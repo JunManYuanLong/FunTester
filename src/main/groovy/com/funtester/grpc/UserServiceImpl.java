c04f90e8aa52d501b53a29985cc5db8ed91f307a
kaiqiang.fan <kaiqiang.fan@okg.com>
2023-12-23 18:26:10 +0800

    upgrade HttpClient 5

--- src/main/groovy/com/funtester/grpc/UserServiceImpl.java
+++ src/main/groovy/com/funtester/grpc/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.funtester.grpc;
 
+import com.funtester.frame.SourceCode;
 import com.funtester.fungrpc.QueryRequest;
 import com.funtester.fungrpc.QueryResponse;
 import com.funtester.fungrpc.UserServiceGrpc;
@@ -10,9 +11,10 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
     @Override
     public void queryUser(QueryRequest request, StreamObserver<QueryResponse> responseObserver) {
         QueryResponse response = QueryResponse.newBuilder()
+                .setId(request.getId())
                 .setName("FunTester")
                 .setAge(18)
-                .setAddress("小八超市2层管理部")
+                .setAddress("小八超市2层管理部" + SourceCode.getRandomInt(100) + "层108号")
                 .setRegion("小八超市")
                 .build();
         responseObserver.onNext(response);
